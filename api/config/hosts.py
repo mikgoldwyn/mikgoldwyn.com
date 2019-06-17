4fc78306ff8b2539f7914e2ffb3109a7c3603e5c
Mik Goldwyn <itsme@mikgoldwyn.com>
2019-06-18 07:07:45 +0800

    TRUE HOSTS

--- api/config/hosts.py
+++ api/config/hosts.py
@@ -3,8 +3,6 @@ from django_hosts import patterns, host
 host_patterns = patterns(
     '',
     host(r'api', 'config.api_urls', name='api'),
-    # host(r'empotech', 'config.empotech_urls', name='empotech'),
-    # host(r'', 'config.default_urls', name='default'),
     host(r'empotech', 'config.empotech_urls', name='empotech'),
-    host(r'', 'config.empotech_urls', name='default'),
+    host(r'', 'config.default_urls', name='default'),
 )
