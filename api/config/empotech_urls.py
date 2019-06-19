0983d4783bc4d61d780a59f2ca7ac81d550b7733
Kevin Marcelo <kevin@marcelo.ph>
2019-06-19 17:03:57 +0800

    Fix routing

--- api/config/empotech_urls.py
+++ api/config/empotech_urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, re_path
 
 
 from django.views.static import serve
@@ -13,5 +13,5 @@ if settings.DEBUG:
     urlpatterns += static('/dist/', document_root=settings.EMPOTECH_DIST_DIR)
 
 urlpatterns += [
-    path('', serve, kwargs={'path': 'index.html', 'document_root': settings.EMPOTECH_DIST_DIR}, name='index')
+    re_path('', serve, kwargs={'path': 'index.html', 'document_root': settings.EMPOTECH_DIST_DIR}, name='index')
 ]
